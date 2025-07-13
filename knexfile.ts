c9922797dbf16592b18a1aea7d727de05622909b
GabMaffei <gabriel.maffei.alm@edu.univali.br>
2025-07-13 19:32:51 -0300

    feat: adicionar migrações para a tabela de transações e configuração do Knex

---
+++ knexfile.ts
@@ -0,0 +1,5 @@
+import { config } from './src/database'
+
+export default config
+
+// npm run knex -- migrate:make create-documents --stub ./node_modules/knex/lib/migrations/migrate/stub/ts.stub
--- src/database.ts
+++ src/database.ts
@@ -1,8 +1,16 @@
-import { knex as setupKnex } from 'knex'
+import { knex as setupKnex, Knex } from 'knex'
 
-export const knex = setupKnex({
+export const config: Knex.Config = {
   client: 'sqlite3',
   connection: {
-    filename: './tmp/app.db',
+    filename: './db/app.db',
   },
-})
+  useNullAsDefault: true,
+  migrations: {
+    extension: '.ts',
+    directory: './db/migrations',
+    stub: './node_modules/knex/lib/migrations/migrate/stub/ts.stub',
+  },
+}
+
+export const knex = setupKnex(config)
