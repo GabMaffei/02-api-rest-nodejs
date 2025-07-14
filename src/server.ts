95df2f511d46bb7bc230ff96e795c251017082ff
GabMaffei <gabriel.maffei.alm@edu.univali.br>
2025-07-13 23:55:53 -0300

    feat: adicionar verificação de sessionId e atualizar rotas de transações para usar session_id

--- src/server.ts
+++ src/server.ts
@@ -1,12 +1,25 @@
 import fastify from 'fastify'
+import cookie from '@fastify/cookie'
 import { env } from './env'
 // import crypto from 'node:crypto'
 import { transactionsRoutes } from './routes/transactions'
 
 const app = fastify()
 
+app.register(cookie)
+
+app.addHook('preHandler', async (request) => {
+  console.log(`[${request.method}] ${request.url}`)
+})
+
 app.register(transactionsRoutes, {
   prefix: 'transactions',
+})
+
+app.get('/hello', async (request, reply) => {
+  return reply.status(200).send({
+    message: 'Hello World!',
+  })
 })
 
 app
